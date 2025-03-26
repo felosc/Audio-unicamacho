c3d41e4312fb4f5b069d409a581e6ce87d803605
felosc <49010129+felosc@users.noreply.github.com>
2025-03-26 14:35:27 +0000

    codigo que lee y muestras las canciones en la carpeta audios- pendiente reproducir

---
+++ cargar_audio.py
@@ -0,0 +1,32 @@
+import pathlib
+import os 
+import pygame
+
+inputdit = 'audios'
+
+intial_count = 0
+
+for path in pathlib.Path(inputdit).iterdir():
+    if path.is_file():intial_count += 1
+
+print('aqui hay ',intial_count,' archivos de uadios\n')
+
+list = os.listdir(inputdit)
+print(list)
+
+song = input('\n ingrese la cancion que quiere reproducir: ')
+file = os.path.join(inputdit, song)
+
+pygame.init()
+pygame.mixer.music.load(file)
+
+while True:
+    n = input(inputdit)
+    if n == "play":
+        pygame.mixer.music.paly()
+    elif n ==  "stop":
+        pygame.mixer.music.pause()
+    elif n == "exit":
+        exit()
+
+
